d206e8a5057802e0e8804f19e10bacef2d087490
SachinC2003 <sachinchougule783@gmail.com>
2026-02-06 22:26:14 +0530

    new changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get("/", (req, res)=>{
-     res.json({message: "Hello World - from new instance"});
+     res.json({message: "Hello World - from new instance. NOw it should work."});
 })
 
 app.listen(PORT, ()=>{
